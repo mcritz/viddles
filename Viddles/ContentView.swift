32f23cbaa08b384e185fd7a5f4f8b065e581c664
Michael Critz <github@michaelcritz.com>
2019-11-27 20:30:22 -0800

    wiring up UI

--- Viddles/ContentView.swift
+++ Viddles/ContentView.swift
@@ -8,9 +8,37 @@
 
 import SwiftUI
 
+struct Meal: Identifiable, Hashable {
+    var id = UUID()
+    var name = "Meal"
+    var noms = "👄👄👄👄"
+}
+
 struct ContentView: View {
+    @State var meals = [
+        Meal(name: "Breakfast", noms: "👄"),
+        Meal(name: "Lunch", noms: "👄👄👄"),
+        Meal(name: "Snack", noms: "👄👄")
+    ]
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Face")
+            VStack {
+                ForEach(meals, id: \.self) { meel in
+                    HStack {
+                        Text(meel.name)
+                        Text(meel.noms)
+                    }
+                }
+            }
+            VStack {
+                Button(action: {
+                    self.meals.append(Meal(name: "Snack", noms: "👄"))
+                }) {
+                    Text("Nom")
+                }
+            }
+        }
     }
 }
 
