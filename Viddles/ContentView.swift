fcf7f6ef20e6d1027491b038854705788af6435f
Michael Critz <github@michaelcritz.com>
2019-11-27 20:08:41 -0800

    Initial Commit

---
+++ Viddles/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Viddles
+//
+//  Created by Michael Critz on 11/27/19.
+//  Copyright © 2019 pixel.science. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
