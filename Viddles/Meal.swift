7333d2f8699404b8efd9ddd857b09395a4cc56fc
Michael Critz <github@michaelcritz.com>
2019-12-15 15:41:08 -0800

    Refactor models away from view

---
+++ Viddles/Meal.swift
@@ -0,0 +1,105 @@
+import Foundation
+import Combine
+
+struct Nom: Codable {
+    let createdAt = Date()
+    var nomValue: Int = 150
+}
+
+struct Meal: Identifiable, Hashable, Equatable, Codable, CustomStringConvertible {
+    let type: MealType
+    var id = UUID()
+    var noms = [Nom]()
+    var description: String {
+        get {
+            let allNomNoms = noms.reduce(into: "") { (res, _) in
+                res.append("🍱")
+            }
+            return "\(type.description)\n\(allNomNoms)"
+        }
+    }
+    
+    static func == (lhs: Meal, rhs: Meal) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    mutating func vomit() {
+        guard noms.count > 0 else { return }
+        noms.removeLast()
+    }
+    
+    mutating func eat(omNom: Nom) {
+        noms.append(omNom)
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id.hashValue)
+    }
+    
+    init(_ type: MealType) {
+        self.type = type
+    }
+}
+
+enum MealType: String, Codable, CustomStringConvertible {
+    case snack, breakfast, lunch, dinner, midnight
+    var description: String {
+        get {
+            return self.rawValue.capitalized(with: Locale.current)
+        }
+    }
+}
+
+class MealDay: Identifiable, ObservableObject, CustomStringConvertible {
+    var description: String {
+        get {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .medium
+            return formatter.string(from: created)
+        }
+    }
+    @Published var meals = [Meal]()
+    let created: Date = Date()
+    
+    func vomit(meal: Meal) {
+        guard let indx = self.meals.firstIndex(of: meal) else { return }
+        var thisMeal = self.meals.remove(at: indx)
+        thisMeal.vomit()
+        if thisMeal.noms.count < 1 { return }
+        self.meals.insert(thisMeal, at: indx)
+    }
+    
+    func eat(nom: Nom) {
+        let hour = Calendar.current.component(.hour, from: Date())
+        var type: MealType
+        switch hour {
+        case 1...5:
+            type = .midnight
+        case 6...9:
+            type = .breakfast
+        case 11...13:
+            type = .lunch
+        case 17...19:
+            type = .dinner
+        default:
+            type = .snack
+        }
+        var didEat = false
+        var existingMeal: Meal
+        for meal in self.meals {
+            if meal.type == type,
+                let idx = self.meals.firstIndex(of: meal) {
+                    existingMeal = meal
+                    self.meals.remove(at: idx)
+                    existingMeal.eat(omNom: Nom())
+                    self.meals.insert(existingMeal, at: idx)
+                    didEat = true
+            }
+        }
+        if !didEat {
+            var newMeal = Meal(type)
+            newMeal.eat(omNom: Nom())
+            self.meals.append(newMeal)
+        }
+    }
+}
