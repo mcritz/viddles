069ec4568c042f324480a0b962d433566872869e
Michael Critz <github@michaelcritz.com>
2019-12-27 20:51:19 -0800

    tests prep

--- ViddlesTests/NomsTests.swift
+++ ViddlesTests/NomsTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import Noms
 
-class ViddlesTests: XCTestCase {
+class NomsTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
